1fab74e313ea878d6fb0665bf564e4486dd7f69e
cd-dist-2324 <cd.dist.2324@gmail.com>
2025-04-02 17:07:01 +0200

    otras posib. en CategoriaRepo

--- src/Repository/CategoriaRepository.php
+++ src/Repository/CategoriaRepository.php
@@ -17,10 +17,25 @@ class CategoriaRepository extends ServiceEntityRepository
     }
 
     public function findAllOrderedByName(): array
-{
-    return $this->createQueryBuilder('c')
-        ->orderBy('c.nombre', 'ASC') // Ordena por nombre en orden ascendente
-        ->getQuery()
-        ->getResult();
-}
+    {
+
+
+
+        return $this->createQueryBuilder('c')
+            ->orderBy('c.nombre', 'ASC') // Ordena por nombre en orden ascendente
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function findAllOrderedByName2(): array
+    {
+//Esto también funcionaría, pero devuelve una colección de arrays de 2 elementos por cada categoría en lugar de un array de objetos Categoría
+        return $this->getEntityManager()->createQuery("SELECT c.nombre, c.id from App\Entity\Categoria c ORDER BY c.nombre")->getResult();
+    }
+
+    public function findAllOrderedByName3(): array
+    {
+//Esto también funcionaría, pero devuelve una colección de arrays de 2 elementos por cada categoría en lugar de un array de objetos Categoría
+        return $this->getEntityManager()->createQuery("SELECT c from App\Entity\Categoria c ORDER BY c.nombre")->getResult();
+    }
 }
